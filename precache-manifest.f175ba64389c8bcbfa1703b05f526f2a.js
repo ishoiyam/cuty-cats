13ce174aef8fa79d4b205a920571b013cb89d54d
ishoiyam <ishoiyam@protonmail.com>
2020-08-01 14:21:32 +0100

    Updates

---
+++ precache-manifest.f175ba64389c8bcbfa1703b05f526f2a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1889714c629d4220651dc8211c3df73b",
+    "url": "/cuty-cats/index.html"
+  },
+  {
+    "revision": "2d325c6b0ad6c008194e",
+    "url": "/cuty-cats/static/css/main.2933f0bd.chunk.css"
+  },
+  {
+    "revision": "dff4d21f00ae84713235",
+    "url": "/cuty-cats/static/js/2.a4281bc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cuty-cats/static/js/2.a4281bc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d325c6b0ad6c008194e",
+    "url": "/cuty-cats/static/js/main.22fd917d.chunk.js"
+  },
+  {
+    "revision": "794568afc152bb23c574",
+    "url": "/cuty-cats/static/js/runtime-main.d949cddd.js"
+  }
+]);
